4255fb857f2eb8c8e69f100a5f035744db6813c8
VEMULA SHANATH KUMAR <65351126+shanathvemula@users.noreply.github.com>
2025-06-04 21:45:14 +0530

    In before commit Added team view and update functionality

--- frontend/src/components/Tournaments/AddTeamDetails/AddTeamDetails.jsx
+++ frontend/src/components/Tournaments/AddTeamDetails/AddTeamDetails.jsx
@@ -12,8 +12,7 @@ const toBase64 = (file) => {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
     reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
-    // .split(',')[1]
+    reader.onload = () => resolve(reader.result);  // .split(',')[1]
     reader.onerror = (error) => reject(error);
   });
 };
